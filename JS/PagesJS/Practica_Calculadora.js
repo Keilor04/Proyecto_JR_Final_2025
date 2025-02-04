a56598c47b9e6c91d28adc939c9837bc8696e2a8
Keilor04 <krodriguezcampos@vlacademy.com>
2025-02-03 20:32:40 -0600

    Se agrega información para resultado de la calculadora en el archivo Practica_Calculadora.js

--- JS/PagesJS/Practica_Calculadora.js
+++ JS/PagesJS/Practica_Calculadora.js
@@ -1,33 +1,42 @@
 
 console.log(`Funcionando Archivo: Practica_Calculadora`);
 
- function enviarDatos() {
+function enviarDatos() {
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
 
 
     //Mostrar se hace con: 
-//Alert, Console log o Inner
-//cl 
-console.log('Mostrar Nombre:',nombre);
-console.log(`Mostrar Edad:`, edad);
-alert (`Nombre: ${nombre} Edad: ${edad}`);
+    //Alert, Console log o Inner
+    //cl 
+    console.log('Mostrar Nombre:', nombre);
+    console.log(`Mostrar Edad:`, edad);
+    alert(`Nombre: ${nombre} Edad: ${edad}`);
 }
 
- function realizarSuma() {
+function realizarSuma() {
     //Por control de calidad se usa la consola, luego se quita al momento de realizar entregar la página web
     console.log(`Funcionando realizarSuma`)
 
+    let numero1
+    let numero2
 
     //con el comando gi se puede obtener el valor de un input de getelementbyid
-    let numero1 = document.getElementById("numero1").value;
-    let numero2 = document.getElementById("numero2").value; 
+    numero1 = document.getElementById("numero1").value;
+    numero2 = document.getElementById("numero2").value;
 
-let resultado = numero1 + numero2;
+    console.log(numero1);
+    console.log(numero2);
 
-console.log(resultado); 
+    let resultado;
 
+    //La funcion parseInt convierte un dato string a un entero
 
+    resultado = parseInt(numero1) + parseInt(numero2);
 
- }
+    console.log(suma)
+//innertext es para mostrar el resultado en la pagina web
+    document.getElementById("resultado").innerHTML = resultado;
+
+}
 
